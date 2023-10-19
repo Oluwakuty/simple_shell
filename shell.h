39946930364440a1d7dd55a632a44b71961b3fe1
oluwakuty <oyindamoanu@gmail.com>
2023-10-18 20:48:24 -0700

    super header

--- shell.h
+++ shell.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,31 +8,10 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 
-extern char **environ;
-
-void writePrompt(void);
-int _strlen(char *s);
-char *_strcpy(char *destination, char *sourcec);
-char *_strdup(char *str);
-int _strcmp(char *str1, char *str2);
-char *_strcat(char *destination, char *source);
-void spaceFree(char **freed);
-char *envGetting(char *naming);
-char *cmdGetting(char *command);
-
-char *readInput(void);
-char **inputSplinter(char *spliter);
-int count_tokinz(char *usrInput);
-int execution(char **command, char **argv);
-
-/* Handle Built-in commands */
-void (*bultin_handler(char *str))(char **, int);
-void env_me(char **command, int statuse);
-void exit_me(char **str, int statuse);
 /**
- * struct bultin - Short description
- * @command: commans string
- * @func: function pointer
+ * struct bultin - Description
+ * @command: string
+ * @func: pointer
  */
 typedef struct bultin
 {
@@ -41,6 +19,25 @@ char *command;
 void (*func)(char **, int);
 } sp_t;
 
+void (*bultin_handler(char *str))(char **, int);
+void env_me(char **command, int statuse);
+void exit_me(char **str, int statuse);
+
+extern char **environ;
+
+void writePrompt(void);
+int _strlen(char *s);
+char *_strcpy(char *destination, char *sourcec);
+char *_strdup(char *str);
+char **inputSplinter(char *spliter);
+int count_tokinz(char *usrInput);
+int execution(char **command, char **argv);
+int _strcmp(char *str1, char *str2);
+char *_strcat(char *destination, char *source);
+void spaceFree(char **freed);
+char *envGetting(char *naming);
+char *cmdGetting(char *command);
+char *readInput(void);
 
 
 #endif
