79c96568d8ba7735dd63fbe4304618171a5fe27c
oluwakuty <oyindamoanu@gmail.com>
2023-10-19 12:32:49 -0700

    simple_sell

--- cmmd.c
+++ cmmd.c
@@ -45,40 +45,35 @@ char *cmdGetting(char *command)
 	char *tokinz;
 	struct stat buff;
 	char *full_path;
-	char *pathCopy;
 
-
+	/* If already path */
+	if (stat(command, &buff) == 0)
+	{
+		return (_strdup(command));
+	}
 	path = envGetting("PATH");
 
 	/* if there's no PATH variable in environ*/
 	if (!path)
 		return (NULL);
-	pathCopy = _strdup(path);
-	tokinz = strtok(pathCopy, ":");
+
+	tokinz = strtok(path, ":");
 
 	while (tokinz)
 	{
-		full_path = malloc((_strlen(tokinz) + _strlen(command) + 2));
+		full_path = malloc(sizeof(char) * (_strlen(tokinz) + _strlen(command) + 2));
 		_strcpy(full_path, tokinz);
 		_strcat(full_path, "/");
 		_strcat(full_path, command);
 		_strcat(full_path, "\0");
 		if (stat(full_path, &buff) == 0)
 		{
-			free(pathCopy);
+			free(path);
 			return (full_path);
 		}
-		else
-		{
-			free(full_path);
-			tokinz = strtok(NULL, ":");
-		}
+		free(full_path);
+		tokinz = strtok(NULL, ":");
 	}
-	free(pathCopy);
-	/* if the command already path */
-	if (stat(command, &buff) == 0)
-	{
-		return (command);
-	}
+	free(path);
 	return (NULL);
 }
